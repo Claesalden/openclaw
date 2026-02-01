549209d56ebfe2d0eb93250ecc4560a78b8c003b
Claesalden <alden.claes@gmail.com>
2026-02-01 23:39:19 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,11 +3,11 @@ set -eu
 
 cd /app
 
-# Render provides PORT. Bind must be non-loopback so Render can reach it.
-# --allow-unconfigured lets it boot even if no ~/.openclaw config exists yet.
 exec node dist/index.js gateway \
+  --dev \
   --allow-unconfigured \
   --port "${PORT:-8080}" \
   --bind lan \
   --auth token \
-  --token "${OPENCLAW_GATEWAY_TOKEN:?set OPENCLAW_GATEWAY_TOKEN in Render}"
+  --token "${OPENCLAW_GATEWAY_TOKEN:?set OPENCLAW_GATEWAY_TOKEN in Render}" \
+  --setup-password "${OPENCLAW_SETUP_PASSWORD:?set OPENCLAW_SETUP_PASSWORD in Render}"
